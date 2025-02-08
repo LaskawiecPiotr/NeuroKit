5ce6a1ccb47ac78513a315b84dae6dfdf1099ac9
LaskawiecPiotr <piotrlaskawiecIB@gmail.com>
2025-02-08 11:56:31 -0500

    Update hrv_nonlinear.py

--- neurokit2/hrv/hrv_nonlinear.py
+++ neurokit2/hrv/hrv_nonlinear.py
@@ -283,8 +283,6 @@ def hrv_nonlinear(peaks, sampling_rate=1000, show=False, **kwargs):
     # Heart Rate Asymmetry
     out = _hrv_nonlinear_poincare_hra(rri, rri_time=rri_time, rri_missing=rri_missing, out=out)
 
-    # DFA
-    out = _hrv_dfa(rri, out, **kwargs)
 
     # Complexity
     tolerance = 0.2 * np.std(rri, ddof=1)
